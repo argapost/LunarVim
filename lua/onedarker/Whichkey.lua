9fa58a7f6b8dacc5b0e7acc92a23278cb6ebd78a
Argyris <arapo4@gmail.com>
2022-03-19 15:31:29 +0100

    clean colorschemes

--- lua/onedarker/Whichkey.lua
+++
@@ -1,9 +0,0 @@
-local Whichkey = {
-  WhichKey = { fg = C.purple },
-  WhichKeySeperator = { fg = C.green },
-  WhichKeyGroup = { fg = C.cyan },
-  WhichKeyDesc = { fg = C.blue },
-  WhichKeyFloat = { bg = C.alt_bg_t },
-}
-
-return Whichkey
